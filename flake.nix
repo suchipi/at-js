f8cb2ab274de2e29b4c1f72a5c5245a1a20d5d29
Lily Scott <git@suchipi.tv>
2022-08-20 21:45:17 -0600

    Add nix flake
    Co-authored-by: Jake Hamilton <jakehamilton@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "at-js (@ binary)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs-16_x ];
+
+          shellHook = ''
+            HI='hiii';
+          '';
+        };
+      }
+    );
+}
